e0e12dabc5975d6dc431e6a52d631d436a11f7a2
andresTrilli <apadilla27@cuc.edu.co>
2019-05-14 20:28:11 -0500

    second commit

--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView lst;
+    private ArrayList<Contact> contacts;
+    private ContactAdapter adapter;
+    private LinearLayoutManager llm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        lst = (RecyclerView)findViewById(R.id.lstcontacts);
+        contacts = new ArrayList<>();
+        contacts.add(new Contact("Lina", "Negrete", "3146399925"));
+        contacts.add(new Contact("Anderson", "Jimenez", "3105552125"));
+        contacts.add(new Contact("Liris", "Arteaga", "3106392013"));
+
+        adapter = new ContactAdapter(MainActivity.this, contacts);
+        llm = new LinearLayoutManager(MainActivity.this);
+        lst.setLayoutManager(llm);
+        lst.setAdapter(adapter);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
